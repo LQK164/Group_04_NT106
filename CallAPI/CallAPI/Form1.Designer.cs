42e967a9a3753a19959072c1d23f74cbca5b7f30
DESKTOP-CTIQ4C3\User <lequockhanh1604@gmail.com>
2023-04-20 23:01:46 +0700

    API

---
+++ CallAPI/CallAPI/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace CallAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtResponse = new RichTextBox();
+            btnGetAll = new Button();
+            btnDelete = new Button();
+            SuspendLayout();
+            // 
+            // txtResponse
+            // 
+            txtResponse.Location = new Point(186, 12);
+            txtResponse.Name = "txtResponse";
+            txtResponse.Size = new Size(602, 426);
+            txtResponse.TabIndex = 3;
+            txtResponse.Text = "";
+            // 
+            // btnGetAll
+            // 
+            btnGetAll.Location = new Point(12, 12);
+            btnGetAll.Name = "btnGetAll";
+            btnGetAll.Size = new Size(141, 80);
+            btnGetAll.TabIndex = 4;
+            btnGetAll.Text = "Get all";
+            btnGetAll.UseVisualStyleBackColor = true;
+            btnGetAll.Click += btnGetAll_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(12, 190);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(141, 80);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDelete);
+            Controls.Add(btnGetAll);
+            Controls.Add(txtResponse);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private RichTextBox txtResponse;
+        private Button btnGetAll;
+        private Button btnDelete;
+    }
+}
---
+++ CallAPI/CallAPI/Rest_Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallAPI
+{
+    public static class Rest_Helper
+    {
+        private static readonly string baseURL = "https://jsonplaceholder.typicode.com/albums";
+
+        public static async Task<string> GetAll()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.GetAsync(baseURL))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        string data = await content.ReadAsStringAsync();
+                        if(data != null)
+                        {
+                            return data;
+                        }    
+                    }    
+                }    
+            }
+            return string.Empty;
+        }
+    }
+}

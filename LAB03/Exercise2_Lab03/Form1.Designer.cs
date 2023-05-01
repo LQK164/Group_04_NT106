5a063e91f09a7b382ef77f2b1ab2fcd9da743d73
LLucasGitHubb <21521826@gm.uit.edu.vn>
2023-05-01 20:08:38 +0700

    Lab03

---
+++ LAB03/Exercise2_Lab03/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Telnet_TCP_listener
+{
+    partial class TCPlistener
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listen = new System.Windows.Forms.Button();
+            this.listViewCommand = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listen
+            // 
+            this.listen.AutoSize = true;
+            this.listen.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.listen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listen.Location = new System.Drawing.Point(460, 55);
+            this.listen.Name = "listen";
+            this.listen.Size = new System.Drawing.Size(107, 32);
+            this.listen.TabIndex = 0;
+            this.listen.Text = "Listen ";
+            this.listen.UseVisualStyleBackColor = false;
+            this.listen.Click += new System.EventHandler(this.listen_Click);
+            // 
+            // listViewCommand
+            // 
+            this.listViewCommand.HideSelection = false;
+            this.listViewCommand.Location = new System.Drawing.Point(22, 104);
+            this.listViewCommand.Name = "listViewCommand";
+            this.listViewCommand.Size = new System.Drawing.Size(545, 325);
+            this.listViewCommand.TabIndex = 1;
+            this.listViewCommand.UseCompatibleStateImageBehavior = false;
+            // 
+            // TCPlistener
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 450);
+            this.Controls.Add(this.listViewCommand);
+            this.Controls.Add(this.listen);
+            this.Name = "TCPlistener";
+            this.Text = "TCP Listener ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button listen;
+        private System.Windows.Forms.ListView listViewCommand;
+    }
+}
+
---
+++ LAB03/Exercise2_Lab03/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets; 
+using System.Threading; 
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Telnet_TCP_listener
+{
+    public partial class TCPlistener : Form
+    {
+        private TCPlistener Server; 
+        public TCPlistener()
+        {
+            InitializeComponent();
+        }
+
+        private void listen_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread ServerThread = new Thread(new ThreadStart(StartUnsafeThread)); 
+            ServerThread.Start();
+        }
+
+        private void StartUnsafeThread ()
+        {
+            int byteReceived = 0;
+            byte[] recv = new byte[1024];
+            Socket clientSocket;
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+            IPEndPoint ipepServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            listenerSocket.Bind(ipepServer);
+            listenerSocket.Listen(-1); 
+            clientSocket = listenerSocket.Accept();
+            listViewCommand.Items.Add(new ListViewItem("New Client Connected"));
+            while(clientSocket.Connected)
+            {
+                string text = " ";
+                do
+                {
+                    byteReceived = clientSocket.Receive(recv);
+                    text += Encoding.ASCII.GetString(recv);
+                    listViewCommand.Items.Add(new ListViewItem(text));
+                }while(text[text.Length - 1] != '\n');
+                listenerSocket.Close(); 
+            }
+        }
+    }
+}

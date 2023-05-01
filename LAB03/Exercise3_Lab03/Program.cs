db9ddecf5a3698494ae6e40aaf58c19846b450ee
Ahamonuser <116050616+Ahamonuser@users.noreply.github.com>
2023-05-01 18:48:59 +0700

    Add files via upload

---
+++ LAB03/Exercise3_Lab03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_server
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Thread server = new Thread(() =>
+            {
+                Application.Run(new Server());
+            });
+            Thread client = new Thread(() =>
+            {
+                Application.Run(new test_client());
+            });
+            server.Start();
+            client.Start();
+        }
+    }
+}

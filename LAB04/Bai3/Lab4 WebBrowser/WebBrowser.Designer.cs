fe7fd5cf1337962b7511eb1e1fdba93467199e94
LLucasGitHubb <21521826@gm.uit.edu.vn>
2023-05-27 10:27:09 +0700

    summit ex3

---
+++ LAB04/Bai3/Lab4 WebBrowser/WebBrowser.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Lab4_WebBrowser
+{
+    partial class WebBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.pnWeb = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnDowHTML = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnViewSource = new System.Windows.Forms.Button();
+            this.pnWeb.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.AutoSize = true;
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(123, 31);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add WebBrowser";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.AutoSize = true;
+            this.btnGo.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnGo.Location = new System.Drawing.Point(132, 3);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(64, 31);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = false;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(202, 3);
+            this.tbAddress.Multiline = true;
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(1075, 31);
+            this.tbAddress.TabIndex = 2;
+            // 
+            // pnWeb
+            // 
+            this.pnWeb.Controls.Add(this.btnAdd);
+            this.pnWeb.Controls.Add(this.btnGo);
+            this.pnWeb.Controls.Add(this.tbAddress);
+            this.pnWeb.Controls.Add(this.btnDowHTML);
+            this.pnWeb.Controls.Add(this.btnViewSource);
+            this.pnWeb.Controls.Add(this.tbPath);
+            this.pnWeb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnWeb.Location = new System.Drawing.Point(0, 0);
+            this.pnWeb.Name = "pnWeb";
+            this.pnWeb.Size = new System.Drawing.Size(1323, 590);
+            this.pnWeb.TabIndex = 3;
+            // 
+            // btnDowHTML
+            // 
+            this.btnDowHTML.AutoSize = true;
+            this.btnDowHTML.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDowHTML.Location = new System.Drawing.Point(3, 40);
+            this.btnDowHTML.Name = "btnDowHTML";
+            this.btnDowHTML.Size = new System.Drawing.Size(123, 31);
+            this.btnDowHTML.TabIndex = 4;
+            this.btnDowHTML.Text = "Download HTML ";
+            this.btnDowHTML.UseVisualStyleBackColor = false;
+            this.btnDowHTML.Click += new System.EventHandler(this.btnDowHTML_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(261, 40);
+            this.tbPath.Multiline = true;
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(1016, 31);
+            this.tbPath.TabIndex = 5;
+            // 
+            // btnViewSource
+            // 
+            this.btnViewSource.AutoSize = true;
+            this.btnViewSource.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnViewSource.Location = new System.Drawing.Point(132, 40);
+            this.btnViewSource.Name = "btnViewSource";
+            this.btnViewSource.Size = new System.Drawing.Size(123, 31);
+            this.btnViewSource.TabIndex = 6;
+            this.btnViewSource.Text = "ViewSource";
+            this.btnViewSource.UseVisualStyleBackColor = false;
+            this.btnViewSource.Click += new System.EventHandler(this.btnViewSource_Click);
+            // 
+            // WebBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1323, 590);
+            this.Controls.Add(this.pnWeb);
+            this.Name = "WebBrowser";
+            this.Text = "Form1";
+            this.pnWeb.ResumeLayout(false);
+            this.pnWeb.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.FlowLayoutPanel pnWeb;
+        private System.Windows.Forms.Button btnDowHTML;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnViewSource;
+    }
+}
+
---
+++ LAB04/Bai3/Lab4 WebBrowser/WebBrowser.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace Lab4_WebBrowser
+{
+    public partial class WebBrowser : Form
+    {
+        System.Windows.Forms.WebBrowser webBrowser;
+        List<string> links = new List<string>();
+        int countlinks = 0;
+        public WebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            webBrowser = new System.Windows.Forms.WebBrowser();
+            webBrowser.Height = 500;
+            webBrowser.Width = 1080;
+            pnWeb.Controls.Add(webBrowser);
+            webBrowser.ScriptErrorsSuppressed = true;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbAddress.Text))
+                webBrowser.Navigate(tbAddress.Text);
+            links.Add(tbAddress.Text);
+            countlinks++;
+        }
+
+        private void btnDowHTML_Click(object sender, EventArgs e)
+        {
+            string url = tbAddress.Text; 
+            string folderPath = tbPath.Text; 
+
+            DownloadWebsiteSource(url, folderPath);
+            MessageBox.Show("Download completed!");
+        }
+
+        private void DownloadWebsiteSource(string url, string folderPath)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = web.Load(url);
+
+            // Lưu trữ nguồn (source) của trang web
+            string source = document.DocumentNode.OuterHtml;
+            string fileName = Path.GetFileName(url);
+            string filePath = Path.Combine(folderPath, fileName + ".html");
+            File.WriteAllText(filePath, source);
+
+            // Download các hình ảnh và file liên quan
+            DownloadSource(document.DocumentNode, folderPath);
+        }
+
+
+        void DownloadSource(HtmlAgilityPack.HtmlNode node, string folderPath)
+        {
+            if (node.NodeType == HtmlAgilityPack.HtmlNodeType.Element)
+            {
+                // Kiểm tra các thẻ HTML img, link và script
+                if (node.Name == "img" || node.Name == "link" || node.Name == "script")
+                {
+                    // Lấy thuộc tính src hoặc href của thẻ
+                    string sourceUrl = node.GetAttributeValue("src", null) ?? node.GetAttributeValue("href", null);
+
+                    if (!string.IsNullOrEmpty(sourceUrl))
+                    {
+                        // Tạo đường dẫn lưu trữ cho tệp tin
+                        string fileName = Path.GetFileName(sourceUrl);
+                        string filePath = Path.Combine(folderPath, fileName);
+
+                        // Tải và lưu tệp tin
+                        using (WebClient client = new WebClient())
+                        {
+                            client.DownloadFile(sourceUrl, filePath);
+                        }
+                    }
+                }
+            }
+            foreach (HtmlAgilityPack.HtmlNode childNode in node.ChildNodes)
+            {
+                DownloadSource(childNode, folderPath);
+            }
+        }
+
+        private void btnViewSource_Click(object sender, EventArgs e)
+        {
+            string url = tbAddress.Text;
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = web.Load(url);
+            string source = document.DocumentNode.OuterHtml;
+            using (Resource viewSourceForm = new Resource())
+            {
+                viewSourceForm.SetSource(source);
+                viewSourceForm.ShowDialog();
+            }
+        }
+    }
+}

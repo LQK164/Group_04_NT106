29ed83cc6a64504b386fc5a251943d23d144540c
hieutrantrong21520859MMCL21 <128446443+hieutrantrong21520859MMCL21@users.noreply.github.com>
2023-05-27 16:33:09 +0700

    Excercise 6-Lab4

---
+++ LAB04/Bai6/Bai6/Page.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai6
+{
+    public partial class frmPage : Form
+    {
+        public frmPage()
+        {
+            InitializeComponent();
+            webBrowser.ScriptErrorsSuppressed = true;
+        }
+
+        public static string news_url;
+
+        private string GetHtml(string url)
+        {
+            string htmlScript = "";
+            WebRequest request = WebRequest.Create(url);
+            using (WebResponse response = request.GetResponse())
+            {
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    htmlScript = reader.ReadToEnd();
+                }
+            }
+            return htmlScript;
+        }
+
+        private void frmPage_Load(object sender, EventArgs e)
+        {
+            string htmlScript = GetHtml(news_url);
+            webBrowser.DocumentText = htmlScript;
+        }
+    }
+}

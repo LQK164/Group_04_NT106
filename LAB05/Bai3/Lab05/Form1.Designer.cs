98f61a8ffd095efac96b822c46d49d89f18df87f
LLucasGitHubb <21521826@gm.uit.edu.vn>
2023-06-11 11:10:52 +0700

    TheAnh's commit

---
+++ LAB05/Bai3/Lab05/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Lab05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.lbTotal = new System.Windows.Forms.Label();
+            this.lbRecent = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(21, 28);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(41, 16);
+            this.lbEmail.TabIndex = 0;
+            this.lbEmail.Text = "Email";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(21, 63);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(67, 16);
+            this.lbPassword.TabIndex = 1;
+            this.lbPassword.Text = "Password";
+            // 
+            // lbTotal
+            // 
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.Location = new System.Drawing.Point(21, 113);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(38, 16);
+            this.lbTotal.TabIndex = 2;
+            this.lbTotal.Text = "Total";
+            // 
+            // lbRecent
+            // 
+            this.lbRecent.AutoSize = true;
+            this.lbRecent.Location = new System.Drawing.Point(223, 113);
+            this.lbRecent.Name = "lbRecent";
+            this.lbRecent.Size = new System.Drawing.Size(50, 16);
+            this.lbRecent.TabIndex = 3;
+            this.lbRecent.Text = "Recent";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(94, 22);
+            this.tbEmail.Multiline = true;
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(256, 22);
+            this.tbEmail.TabIndex = 4;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(94, 57);
+            this.tbPassword.Multiline = true;
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(256, 22);
+            this.tbPassword.TabIndex = 5;
+            // 
+            // btLogin
+            // 
+            this.btLogin.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btLogin.Location = new System.Drawing.Point(525, 28);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(210, 40);
+            this.btLogin.TabIndex = 6;
+            this.btLogin.Text = "Login";
+            this.btLogin.UseVisualStyleBackColor = false;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(711, 263);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.lbRecent);
+            this.Controls.Add(this.lbTotal);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbEmail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Label lbTotal;
+        private System.Windows.Forms.Label lbRecent;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LAB05/Bai3/Lab05/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenPop.Mime;
+using OpenPop.Pop3;
+
+namespace Lab05
+{
+    //email: anhngo051203@gmail.com
+    //pwd: fwhpgnazychvlcnc
+    public partial class Form1 : Form
+    {
+        private Pop3Client pop3Client;
+        private List<OpenPop.Mime.Message> messages;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            string server = "pop.gmail.com"; // Địa chỉ máy chủ POP3 của Gmail
+            int port = 995; // Cổng kết nối POP3 của Gmail
+            string username = tbEmail.Text; // Tên người dùng của email
+            string password = tbPassword.Text; // Mật khẩu của email
+            int limit = 5; // Số lượng email muốn hiển thị
+
+            pop3Client = new Pop3Client();
+
+            try
+            {
+                pop3Client.Connect(server, port, true); // Kết nối tới máy chủ POP3
+                pop3Client.Authenticate(username, password); // Xác thực đăng nhập vào email
+
+                int messageCount = pop3Client.GetMessageCount(); // Lấy số lượng email trong hộp thư
+                int startIndex = messageCount - limit + 1; // Vị trí bắt đầu lấy email
+                int endIndex = messageCount; // Vị trí kết thúc lấy email
+
+                // Tạo các cột cho DataGridView hiển thị thông tin email
+                dataGridView1.Columns.Add("Subject", "Subject");
+                dataGridView1.Columns.Add("From", "From");
+                dataGridView1.Columns.Add("Date", "Date");
+                dataGridView1.Columns["Subject"].ReadOnly = true;
+                dataGridView1.Columns["From"].ReadOnly = true;
+                dataGridView1.Columns["Date"].ReadOnly = true;
+
+                // Lặp qua các email từ vị trí bắt đầu đến vị trí kết thúc
+                for (int i = endIndex; i >= startIndex; i--)
+                {
+                    OpenPop.Mime.Message message = pop3Client.GetMessage(i); // Lấy thông tin của email
+                    string subject = message.Headers.Subject; // Lấy chủ đề của email
+                    string from = message.Headers.From.Address; // Lấy địa chỉ người gửi của email
+                    DateTime date = message.Headers.DateSent; // Lấy ngày gửi của email
+
+                    // Thêm thông tin của email vào DataGridView
+                    dataGridView1.Rows.Add(subject, from, date);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message); // Hiển thị thông báo lỗi nếu có lỗi xảy ra
+            }
+            finally
+            {
+                if (pop3Client != null && pop3Client.Connected)
+                    pop3Client.Disconnect(); // Ngắt kết nối sau khi hoàn thành việc lấy email
+            }
+        }
+    }
+}
